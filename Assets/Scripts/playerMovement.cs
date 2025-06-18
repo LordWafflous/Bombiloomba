55e4ae844d26e5e7080a82a83c752e48c9adb13a
LordWafflous <elifbatci@gmail.com>
2025-06-18 07:43:11 +0300

    Inventory Open/Close
    - With options hold and toggle

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -15,9 +15,12 @@ public class playerMovement : MonoBehaviour
 
     [Header("Components")]
     public float speed;
+    public GameObject inventoryPanel;
+
+    [Header("Settings")]
+    public bool holdI = false;
 
     private Rigidbody2D player;
-    
 
     void Start()
     {
@@ -26,10 +29,12 @@ public class playerMovement : MonoBehaviour
 
         shiftRun.AddListener(shiftRunFunc);
         sceneChange.AddListener(clickDoor);
+        
 
         Debug.Log("Yippie");
-    }
 
+        inventoryPanel.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
@@ -38,10 +43,10 @@ public class playerMovement : MonoBehaviour
 
         Vector2 direction = new Vector2(horizontalInput, verticalInput);
 
-        player.velocity = direction*speed;
+        player.velocity = direction * speed;
         player.constraints = RigidbodyConstraints2D.FreezeRotation;
 
-        if(Input.GetKey(KeyCode.LeftShift) && shiftRun != null)
+        if (Input.GetKey(KeyCode.LeftShift) && shiftRun != null)
         {
             shiftRun.Invoke();
         }
@@ -49,30 +54,54 @@ public class playerMovement : MonoBehaviour
         {
             speed = 4;
         }
-    }
 
-     void OnCollisionEnter2D(Collision2D col)
-     {
-        //Debug.Log("Collision");
+        handleInventory();
         
-     }
+        
 
-
-     void OnTriggerStay2D(Collider2D col)
-     {
-        if(col.gameObject.CompareTag("door") && (Input.GetKey(KeyCode.Mouse1)))
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Debug.Log("Collision");
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        Debug.Log("Triggered Door!");
+        if (col.gameObject.CompareTag("door") && Input.GetKeyDown(KeyCode.Mouse1) && sceneChange != null)
         {
             sceneChange.Invoke();
         }
-     }
+    }
 
-     public void shiftRunFunc()
-     {
+    void handleInventory()
+    {
+        if (!holdI && Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (inventoryPanel.activeSelf)
+            {
+                inventoryPanel.SetActive(false);
+            }
+            else
+            {
+                inventoryPanel.SetActive(true);
+            }
+        }
+        else if (holdI && Input.GetKey(KeyCode.Tab)) //put the do function
+        {
+            inventoryPanel.SetActive(true);
+        }
+        else if(holdI)
+        {
+            inventoryPanel.SetActive(false);
+        }
+    }
+    public void shiftRunFunc()
+    {
         speed = 6;
-     }
-    
+    }
     public void clickDoor()
     {
-        SceneManager.LoadScene("wetRuins");
+        SceneManager.LoadScene(2); //Wet Ruins
     }
+    
 }
